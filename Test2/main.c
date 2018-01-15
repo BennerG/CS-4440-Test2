2b6013893a70be3616c9b560771a6f9114720f84
Ben Gurrola <bgurrola.btg@gmail.com>
2018-01-15 03:25:05 -0700

    added initial project

---
+++ Test2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Test2
+//
+//  Created by Benjamin Thomas Gurrola on 1/15/18.
+//  Copyright © 2018 Benjamin Thomas Gurrola. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
